f5a09abb9da96954db5240e1c50d21320a82e394
JohnsonLiu25 <liuj25@rpi.edu>
2016-07-11 15:08:52 -0400

    test

--- redis_test.js
+++ redis_test.js
@@ -16,8 +16,8 @@ app.get('/', function(request, response) {
     response.render('pages/index');
 });
 
-app.get('/Johnson', function(req,res){
-    res.send('dog');
+app.get('/', function(req,res){
+    res.send(dog);
 } 
 );
 
@@ -25,7 +25,7 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-
+dog = 'hello'
 /*
 var types = [];
 var locations = [];
@@ -125,7 +125,7 @@ client.sadd("senderid4",JSON.stringify({'type':'villa',
         'maxPrice' : 2340,
         'beds' : 3}));
 client.smembers("senderid2", function(err, reply){
-	console.log(reply);
+	dog += reply
 })
 
 //grabKeys();
