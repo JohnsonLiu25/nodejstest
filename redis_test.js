771a2d5619abc6634273e266c4024a01bef5a678
JohnsonLiu25 <liuj25@rpi.edu>
2016-07-11 15:07:44 -0400

    test

--- redis_test.js
+++ redis_test.js
@@ -1,6 +1,31 @@
 //Analyze entire database
 var client = require('redis').createClient(process.env.REDIS_URL);
+
 var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.get('/Johnson', function(req,res){
+    res.send('dog');
+} 
+);
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
 /*
 var types = [];
 var locations = [];
